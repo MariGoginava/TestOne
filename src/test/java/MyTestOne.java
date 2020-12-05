012f0194195584b8c988045ed4dcff3448d736ba
MariGoginava <marigoginava2815@gmail.com>
2020-12-05 16:21:24 +0400

    Initial commit on TestOne Try 4

---
+++ src/test/java/MyTestOne.java
@@ -0,0 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyTestOne {
+    @Test
+    public void openLink() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(" http://the-internet.herokuapp.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Forgot Password")).click();
+        WebElement email = driver.findElement(By.xpath("//input[@type='text']"));
+        email.sendKeys("Example@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"form_submit\"]/i")).click();
+        driver.quit();
+    }
+}
